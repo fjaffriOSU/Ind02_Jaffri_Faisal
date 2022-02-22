81db706947f1adc67d2e5be25ad00f2256b6c389
Faisal Jaffri <jeffaiz72@gmail.com>
2022-02-21 22:46:38 -0600

    Initial Commit

---
+++ Ind02_Jaffri_Faisal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind02_Jaffri_Faisal
+//
+//  Created by Faisal Jaffri on 2/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
